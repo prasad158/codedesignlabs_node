66fdeb083bde96a851a47190a584fa23c02597d2
Prasad <prasadbamane93@gmail.com>
2021-08-13 23:15:49 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+
+import router from "@routes/router";
+
+dotenv.config({ path: path.resolve('.env') });
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on PORT ${process.env.PORT}`);
+});
+
+app.use('/api/gtw', router)
