d4153a806010d7af3c26bb1a2fdc0b11a7972fbe
Prasad <prasadbamane93@gmail.com>
2021-09-04 19:59:49 +0530

    jenkins and docker file added

--- backend/src/common/middlewares/response.middleware.ts
+++ backend/src/common/middlewares/response.middleware.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 export default (req: Request, res: Response, next: NextFunction) => {
     res.sendRes = (status: number, body) => {
+        console.log(req);
+        console.log(res);
         res.status(status).send(body);
     }
 
