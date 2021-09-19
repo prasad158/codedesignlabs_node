ef7043a70cad88e216b78aea04db6adb99b0fb2c
Prasad <prasadbamane93@gmail.com>
2021-09-19 17:07:16 +0530

    api call added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private _http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this._http.post('http://localhost:8082/api/gtw/auth/login', {
+      "email": "n@codesignlabs.com",
+      "password": "Neha@2307#"
+    }).subscribe((data: any) => {
+      console.log(data);
+    });
+  }
 }
